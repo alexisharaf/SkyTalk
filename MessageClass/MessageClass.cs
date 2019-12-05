1443c95fe09dca40226763f034320f63b316cf9e
alexisharaf <alexi.sharaf@gmail.com>
2019-12-05 14:11:03 +0700

    Класс сообщений выделен в отдельную библиотеку

---
+++ MessageClass/MessageClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SkyTalk
+{
+
+    [Serializable()]
+    public class MessageClass
+    {
+
+        public string User;
+        public string Password;
+        public string Command;
+        public string Data;
+
+    }
+}
