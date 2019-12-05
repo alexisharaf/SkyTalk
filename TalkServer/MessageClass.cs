074b18d397025dc5b0f5bf25e94aad29c049e18f
alexisharaf <alexi.sharaf@gmail.com>
2019-12-05 14:00:42 +0700

    ser

---
+++ TalkServer/MessageClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SkyTalk
+{
+    [Serializable]
+    class MessageClass
+    {
+        public string User;
+        public string Password;
+        public string Command;
+        public string Data;
+
+    }
+}
